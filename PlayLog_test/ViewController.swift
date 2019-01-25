3c6c739853b56eb32066894c204a78c8cf332947
Rupika Sompalli <rupikasompalli@rupikas-mac-mini.local>
2019-01-25 07:08:17 -0500

    Initial Commit

---
+++ PlayLog_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayLog_test
+//
+//  Created by Rupika Sompalli on 25/01/19.
+//  Copyright © 2019 Venkata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
